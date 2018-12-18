5a869c09d97a3f1b413150ff10171a67240d3943
Роман Мазин <mazin.roma1992@yandex.ru>
2018-12-18 12:25:45 +0300

    Что то изменил...

---
+++ KSED/Tests/SZ/test_SZ.py
@@ -0,0 +1,36 @@
+#!/bin/sh
+#!/usr/bin/python3
+# -*- encoding=utf8 -*-
+
+
+
+# How to run:
+
+#.... python -m pytest -v --driver Chrome --driver-path WebDriver\chromedriver --alluredir ./allure_report
+#.... allure generate ./allure_report && allure open allure-report
+# -s команда вывода всех print в консоль
+
+
+
+import pytest
+import allure
+
+from Tests.SZ.tk11691 import KSEDCreatDocSZ
+from Tests.SZ.tk12913 import KSEDprintSZ
+
+
+
+
+@allure.feature('Создание Служебной записки')
+
+@pytest.mark.KSED_smoke_test
+
+def test_11691(web_browser):
+
+    """ Создание Служебной записки. """
+
+    page = KSEDCreatDocSZ(web_browser)
+
+    LogIn_page = page.LogIN('StroganovSN', '12345')
+
+    Creat_doc  = page.Creat()
